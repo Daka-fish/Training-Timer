4fca475795c142098341d8f7e93c7f2da2934357
Daka-fish <yamatohidaka24@gmail.com>
2025-03-12 22:25:50 +0900

    edit button (start, stop, resume, reset)

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let TIMER_SECONDS = 0;
 let CURRENT_TIMER_SECONDS = 0;
 let TIMER;
 
-function get_input_number() {
+let TIMER_STATE = 0; //0:wait, 1:running, 2:stopped
+
+function get_input_number(){
   let raw_number = document.getElementById("timer-seconds").value;
 
   if (raw_number == "") return 0;
@@ -17,8 +19,30 @@ function set_seconds_on_display(seconds){
   return 0;
 }
 
-function start_timer() {
+function control_timer(){
+  let control_button = document.getElementById("control-btn");
+  switch(TIMER_STATE){
+    case 0:
+      start_timer();
+      TIMER_STATE = 1;
+      control_button.innerText = "停止";
+      break;
+    
+    case 1:
+      stop_timer();
+      TIMER_STATE = 2;
+      control_button.innerText = "再開";
+      break;
 
+    case 2:
+      start_count_down();
+      TIMER_STATE = 1;
+      control_button.innerText = "停止";
+      break;
+  }
+}
+
+function start_timer(){
   if(TIMER != null) clearInterval(TIMER);
 
   let input_number = get_input_number();
@@ -31,14 +55,36 @@ function start_timer() {
   CURRENT_TIMER_SECONDS = TIMER_SECONDS;
 
   set_seconds_on_display(TIMER_SECONDS);
+  start_count_down();
+}
 
+function stop_timer(){
+  if(TIMER_STATE!=1) return;
+  clearInterval(TIMER);
+  set_seconds_on_display(CURRENT_TIMER_SECONDS);
+  TIMER_STATE = 2;
+}
+
+function start_count_down(){
   TIMER = setInterval(() => {
     CURRENT_TIMER_SECONDS--;
     set_seconds_on_display(CURRENT_TIMER_SECONDS);
 
-    if(CURRENT_TIMER_SECONDS <= 0){
+    if(CURRENT_TIMER_SECONDS < 0){
       clearInterval(TIMER);
+      TIMER_STATE = 1;
+      document.getElementById("control-btn").innerText = "開始";
+      set_seconds_on_display(TIMER_SECONDS);
     }
 
   },1000);
+}
+
+function reset_timer(){
+  if(TIMER_STATE == 0) return;
+  set_seconds_on_display(TIMER_SECONDS);
+  CURRENT_TIMER_SECONDS = TIMER_SECONDS;
+  TIMER_STATE = 0;
+  document.getElementById("control-btn").innerText = "開始";
+  clearInterval(TIMER);
 }
